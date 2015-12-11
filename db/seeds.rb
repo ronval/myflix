dec4cf8e3b2749a1bd27aa92ee52def1ec88d863
Ron <Ron@Rons-MacBook.local>
2015-12-11 14:29:02 -0500

    added video controller, model, and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Video.create(title:"Batman v Superman", description:"Mighty Heros fighting", small_cover_url:"/tmp/batmanvsuperman.jpg" )
+Video.create(title:"Starwars: Force Awakens", description:"BEST MOVIE EVERRRRRR", small_cover_url:"/tmp/starwars.jpg" )
+Video.create(title:"Captain America: Civil War", description:"Mighty Heros fighting", small_cover_url:"/tmp/civilwar.jpg" )
+Video.create(title:"Wonder Woman", description:"Mighty Hero", small_cover_url:"/tmp/wonderwoman.jpg" )
+Video.create(title:"TMNT", description:"Turtles Power", small_cover_url:"/tmp/tmnt.jpg")
+

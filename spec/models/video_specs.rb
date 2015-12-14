65acf6567478bb552d9b91984c199803c34c8d51
Ron <Ron@Rons-MacBook.local>
2015-12-14 14:57:16 -0500

    Added test for video model for saving objects to DB

--- spec/models/video_specs.rb
+++ spec/models/video_specs.rb
@@ -4,7 +4,9 @@ require 'spec_helper'
 
 describe Video do 
   it "should save itself to the database" do 
-    
+    vid = Video.new(title:"Rogue One", description:"Star wars fighter pilots", small_cover_url:"/tmp/batmanvsuperman.jpg", large_cover_url:"/tmp/monk_large.jpg", category_id: 3 )
+    vid.save
+    expect(Video.last.title).to eql("Rogue One")
   end 
 
 end 

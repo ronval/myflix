fa9cd078cf04c946df41cb9d63ca3d11e445cff2
Ron <ronald.m.valencia@gmail.com>
2016-04-15 14:35:24 -0400

    fixed typo

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,5 @@
 CarrierWave.configure do |config|
-  if Rals.env.staging? || Rails.env.production?
+  if Rails.env.staging? || Rails.env.production?
     config.storage    = :aws
     config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
     config.aws_acl    = 'public-read'

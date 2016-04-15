2d90aecea76b6627a71a95a7d560a75bcfcca41b
Ron <ronald.m.valencia@gmail.com>
2016-04-15 14:02:08 -0400

    added upload gems configs

---
+++ app/controllers/admin/videos_controller.rb
@@ -0,0 +1,34 @@
+class Admin::VideosController <ApplicationController
+  before_action :require_user
+  before_action :require_admin
+  
+  def new
+    @video =Video.new
+  end
+
+  def create
+    @video = Video.new(video_params)
+    binding.pry
+    if @video.valid?
+      @video.save
+      redirect_to new_admin_video_path
+      flash[:notice] = "You have created a new video"
+    else
+      render :new
+        flash[:notice] ="You missed some information"
+    end 
+  end
+
+  private 
+    def require_admin
+      if !current_user.admin?
+      redirect_to home_path 
+      flash[:notice] = "You are not authorized to access that"
+      end 
+    end
+
+    def video_params
+      params.require(:video).permit(:title,:category_id,:description, :small_cover, :large_cover)
+    end
+
+end 
